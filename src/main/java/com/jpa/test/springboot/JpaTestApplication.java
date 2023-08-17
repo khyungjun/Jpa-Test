97020dd773255cfe09167bc30ae6647a586edbd5
khyungjun <kpos7503@naver.com>
2023-08-17 17:06:07 +0900

    Init Project

---
+++ src/main/java/com/jpa/test/springboot/JpaTestApplication.java
@@ -0,0 +1,14 @@
+package com.jpa.test.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaTestApplication {
+
+	// Âü°í : https://velog.io/@sheltonwon/Spring-Boot-MariaDB-JPA-%EA%B0%84%EB%8B%A8-%EC%98%88%EC%8B%9C
+	public static void main(String[] args) {
+		SpringApplication.run(JpaTestApplication.class, args);
+	}
+
+}
