98f2b68b81fd2ccaad3bf52276557aeb4160afde
khyungjun <kpos7503@naver.com>
2023-11-17 18:58:17 +0900

    change package name

--- src/main/java/com/jpa/test/springboot/web/domain/Test.java
+++ src/main/java/com/jpa/test/springboot/web/domain/Test.java
@@ -1,4 +1,4 @@
-package com.jpa.test.springboot.web.entity;
+package com.jpa.test.springboot.web.domain;
 
 import javax.persistence.*;
 
@@ -15,7 +15,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @EnableJpaAuditing
 @EntityListeners(AuditingEntityListener.class)
-public class Test {
+public class Test { //출처 : https://velog.io/@sheltonwon/Spring-Boot-MariaDB-JPA-%EA%B0%84%EB%8B%A8-%EC%98%88%EC%8B%9C
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(nullable = false)
--- src/main/java/com/jpa/test/springboot/web/repository/TestRepository.java
+++ src/main/java/com/jpa/test/springboot/web/repository/TestRepository.java
@@ -5,7 +5,7 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.jpa.test.springboot.web.entity.Test;
+import com.jpa.test.springboot.web.domain.Test;
 
 @Repository
 public interface TestRepository extends JpaRepository<Test, Integer> {
